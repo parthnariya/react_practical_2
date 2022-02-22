cb2d8680bc24f6fdfc4217f74d94685620f9ab9a
parthnariya <68768212+parthnariya@users.noreply.github.com>
2022-02-22 15:10:33 +0530

    created react app using tool chain

---
+++ reactMyApp/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./main.js",
+  output: {
+    path: path.join(__dirname, "/bundle"),
+    filename: "index_bundle.js",
+  },
+  devServer: {
+    port: 8001,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+};
